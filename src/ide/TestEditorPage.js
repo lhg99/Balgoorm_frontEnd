d6d521d328ddb7cfedf6c899f0032c73356b0d8a
mingi choo <cn78943@gmail.com>
2024-06-21 17:37:05 +0900

    test

--- src/ide/TestEditorPage.js
+++ src/ide/TestEditorPage.js
@@ -1,28 +1,63 @@
 import React, { useRef, useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
 import Editor from '@monaco-editor/react';
 import { Button, Tabs, Tab } from 'react-bootstrap';
-import Modal from '../components/Modal';
 import axios from 'axios';
 import './EditorPage.css';
+import ResponseModal from '../components/modal/Modal';
+import ArrowRightIcon from '../components/icons/ArrowRightIcon';
+import ArrowLeftIcon from '../components/icons/ArrowLeftIcon';
 
-function TestEditorPage() {
+const defaultValues = {
+  JAVA: `public class Main {
+    public static void main(String[] args) {
+        System.out.print(15); 
+    }
+}`,
+
+  PYTHON: `def add_numbers(a, b):
+    return a + b
+
+result = add_numbers(10, 5)
+print(result, end='')`,
+
+  CPP: `#include <iostream>
+
+int addNumbers(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int result = addNumbers(10, 5);
+    std::cout << result;  // 줄바꿈 없이 출력: 15
+    return 0;
+}
+`
+
+};
+
+function EditorPage() {
+  const { id } = useParams(); // 퀴즈 ID에 해당
   const editorRef = useRef(null);
   const [output, setOutput] = useState('');
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [modalMessage, setModalMessage] = useState('');
-  const [problem, setProblem] = useState({ name: '', content: '', detail: '' });
+  const [responseData, setResponseData] = useState({});
+  const [quiz, setQuiz] = useState({ quiz_title: '', quiz_content: '', quiz_reg_date: '' });
+  const [selectedLanguage, setSelectedLanguage] = useState("JAVA");
+  const [editorValue, setEditorValue] = useState(defaultValues.JAVA);
 
   useEffect(() => {
-    axios.get('http://localhost:8080/api/problems/1')
+    axios.get(`http://localhost:8080/api/quiz/detail/${id}?userId=1`)
       .then(response => {
+        console.log('response :', response);
         const data = response.data;
         console.log('Fetched problem data:', data);
-        setProblem(data);
+        setQuiz(data);
       })
       .catch(error => {
         console.error('Error fetching problem data:', error);
       });
-  }, []);
+  }, [id]);
 
   function handleEditorDidMount(editor, monaco) {
     editorRef.current = editor;
@@ -31,66 +66,63 @@ function TestEditorPage() {
   function runCode() {
     if (editorRef.current) {
       const code = editorRef.current.getValue();
-      try {
-        // eslint-disable-next-line no-eval
-        const result = eval(code);
-        setOutput(result);
-        checkAnswer(result);
-      } catch (error) {
-        setOutput(error.toString());
-        setModalMessage('오답');
-        setIsModalOpen(true);
-      }
+      const requestData = {
+        quizId: id,
+        language: selectedLanguage,
+        code: code,
+        userId: 1
+      };
+      console.log("reqData :", requestData);
+      axios.post("http://localhost:8080/api/ide/run", requestData)
+        .then(response => {
+          const data = response.data;
+          console.log("response data:", data);
+          setOutput(data.result);
+          setResponseData(data);
+          setIsModalOpen(true);
+        })
+        .catch(error => {
+          console.error("Error:", error);
+        });
     }
-  }
-
-  function checkAnswer(userOutput) {
-    axios.post('http://localhost:8080/api/problems/check', {
-      problemId: 1,
-      userOutput: userOutput
-    })
-      .then(response => {
-        const isCorrect = response.data;
-        setModalMessage(isCorrect ? '정답' : '오답');
-        setIsModalOpen(true);
-      })
-      .catch(error => {
-        console.error('Error checking answer:', error);
-        setModalMessage('오답');
-        setIsModalOpen(true);
-      });
   }
 
   function closeModal() {
     setIsModalOpen(false);
   }
 
+  function handleLanguageChange(event) {
+    const selectedLang = event.target.value;
+    setSelectedLanguage(selectedLang);
+    setEditorValue(defaultValues[selectedLang]);
+  }
+
   return (
     <div className="editor-page">
       <div className="problem-container">
-        <Tabs defaultActiveKey="problem" id="problem-tabs" >
-          <Tab eventKey="basic" title="기본 개념">
-            <div className="">
-d
+        <Tabs defaultActiveKey="basic" id="problem-tabs">
+          <Tab eventKey="basic" title="기본 개념" className="tab-content">
+            <div className="basic-content">
+              기본 개념 내용
             </div>
           </Tab>
           <Tab eventKey="problem" title="문제" className="tab-content">
-              <div className="problem-name">
-                {problem.name}
+            <div className="problem-name">
+              {quiz.quizTitle}
+            </div>
+            <div className="problem-content">
+              {quiz.quizContent ? quiz.quizContent.split('\\n').map((line, index) => (
+                <div key={index}>{line}</div>
+              )) : 'Loading...'}
+            </div>
+            <div className="problem-detail">
+              <div className="problem-detail-name">
+                주의사항? 입출력예제?
               </div>
-              <div className="problem-content">
-                {problem.content ? problem.content.split('\n').map((line, index) => (
-                  <div key={index}>{line}</div>
-                )) : 'Loading...'}
-              </div>
-              <div className="problem-detail">
-                <div className="problem-detail-name">
-                  주의사항? 입출력예제?
-                </div>
-                {problem.detail ? problem.detail.split('\n').map((line, index) => (
-                  <div key={index}>{line}</div>
-                )) : 'Loading...'}
-              </div>
+              {quiz.quizRegDate ? quiz.quizRegDate.split('\\n').map((line, index) => (
+                <div key={index}>{line}</div>
+              )) : 'Loading...'}
+            </div>
           </Tab>
           <Tab eventKey="qnaboard" title="질의응답">
             <div className="placeholder-content">
@@ -101,20 +133,54 @@ d
       </div>
 
       <div className="editor-container">
+
+        <div className="horizontal-menu">
+          <div>언어 선택</div>
+          <div className="language-container">
+            <div>
+              <input
+                type="radio"
+                id="java"
+                name="language"
+                value="JAVA"
+                checked={selectedLanguage === "JAVA"}
+                onChange={handleLanguageChange}
+              />
+              <label htmlFor="java">Java</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="python"
+                name="language"
+                value="PYTHON"
+                checked={selectedLanguage === "PYTHON"}
+                onChange={handleLanguageChange}
+              />
+              <label htmlFor="python">Python</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="cpp"
+                name="language"
+                value="CPP"
+                checked={selectedLanguage === "CPP"}
+                onChange={handleLanguageChange}
+              />
+              <label htmlFor="cpp">C++</label>
+              <ArrowLeftIcon />
+            </div>
+          </div>
+        </div>
+
         <div className="editor-box">
           <Editor
             width="100%"
             height="100%"
-            language="javascript"
+            language={selectedLanguage.toLowerCase()}
             theme="vs-dark"
-            defaultValue={`// 기본 사용법 작성
-//~~~~~~~~
-// 예제: 두 숫자의 합을 계산하여 반환하는 함수
-function add(a, b) {
-  return a + b;
-}
-
-add(5, 10);`}
+            value={editorValue}
             onMount={handleEditorDidMount}
             options={{
               padding: {
@@ -127,13 +193,22 @@ add(5, 10);`}
           <Button variant="outline-dark" onClick={runCode}>답안제출</Button>
         </div>
         <div className="output-container">
-          <div className="output-title">Output:</div>
-          <pre>{output}</pre>
+          <div className="output-title">Output: {output}</div>
+          <p><strong>Answer:</strong> {responseData.answer}</p>
+          <p><strong>Correct:</strong> {responseData.correct ? 'True' : 'False'}</p>
+          <p><strong>Error Message:</strong> {responseData.errorMessage || 'None'}</p>
+          <p><strong>Memory Usage:</strong> {responseData.memoryUsage}</p>
+          <p><strong>Result:</strong> {responseData.result}</p>
+          <p><strong>Run Time:</strong> {responseData.runTime}</p>
         </div>
-        {isModalOpen && <Modal message={modalMessage} onClose={closeModal} />}
+        <ResponseModal
+          show={isModalOpen}
+          onHide={closeModal}
+          responseData={responseData}
+        />
       </div>
     </div>
   );
 }
 
-export default TestEditorPage;
+export default EditorPage;
