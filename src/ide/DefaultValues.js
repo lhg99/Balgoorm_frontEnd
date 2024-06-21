80a5914f702caa2417f3abe95baa9509dd54bfe0
mingi choo <cn78943@gmail.com>
2024-06-21 17:35:40 +0900

    :hammer: [FE] 에디터 기본 텍스트 변수화

---
+++ src/ide/DefaultValues.js
@@ -0,0 +1,32 @@
+const defaultValues = {
+    JAVA: `//선택 된 언어 : Java
+    public class Main {
+      public static void main(String[] args) {
+          System.out.print(15); 
+      }
+  }`,
+  
+    PYTHON: `#선택 된 언어 : Python
+    def add_numbers(a, b):
+      return a + b
+  
+  result = add_numbers(10, 5)
+  print(result, end='')`,
+  
+    CPP: `//선택 된 언어 : C++
+    #include <iostream>
+  
+  int addNumbers(int a, int b) {
+      return a + b;
+  }
+  
+  int main() {
+      int result = addNumbers(10, 5);
+      std::cout << result;  // 줄바꿈 없이 출력: 15
+      return 0;
+  }
+  `
+  };
+  
+  export default defaultValues;
+  
