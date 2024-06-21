f66e500c049f18140a30c0fef88ed803e5fbd646
mingi choo <cn78943@gmail.com>
2024-06-21 17:36:54 +0900

    :sparkles: [FE] 문제 리스트 페이징 추가

--- src/quiz/QuizList.js
+++ src/quiz/QuizList.js
@@ -1,38 +1,67 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
-import { ListGroup, Container } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
+import './QuizList.css';
+
+
 
 function QuizList() {
-  const [quiz, setquiz] = useState([]);
+  const [quiz, setQuiz] = useState([]);
+  const [page, setPage] = useState(1);
+
+  const fetchData = async (pageNumber) => {
+    try {
+      console.log("요청시작");
+      const response = await axios.get(`http://localhost:8080/api/quiz/list/${pageNumber}`);
+      console.log("요청완료 :" + JSON.stringify(response));
+      setQuiz(response.data);
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        console.log("요청시작")
-        const response = await axios.get('http://localhost:8080/quiz/list/1');
-        console.log("요청완료")
-        setquiz(response.data);
-      } catch (error) {
-        console.error('Error fetching data:', error);
-      }
-    };
+    fetchData(page);
+  }, [page]);
 
-    fetchData();
-  }, []);
+  const handlePreviousPage = () => {
+    if (page > 1) {
+      setPage(page - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    setPage(page + 1);
+  };
 
   return (
-    <div>
-      <div>
-        문제 목록
+    <div className="quiz-page">
+      <div className="quiz-title-container">
+        <div>문제 목록</div>
       </div>
-      <ListGroup>
-        {quiz.map((quiz, index) => (
-          <ListGroup.Item key={index}>
-            <Link to={`/quiz/detail/${quiz.quizId}`}>{quiz.quizTitle}</Link>
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
+
+      <div className="quiz-container">
+        <ul>
+          {quiz.map((quizItem, index) => (
+            <li key={index}>
+              <Link to={`/quiz/detail/${quizItem.quizId}`}>
+                {quizItem.quizTitle}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+
+      <div className="list-container">
+        <Button variant="primary" onClick={handlePreviousPage} disabled={page === 1}>
+          이전
+        </Button>
+        <span>페이지 {page}</span>
+        <Button variant="primary" onClick={handleNextPage}>
+          다음
+        </Button>
+      </div>
     </div>
   );
 }
