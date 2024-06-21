4bab7c5a96c981214ee2043748d462d4dc8617e7
mingi choo <cn78943@gmail.com>
2024-06-21 17:34:24 +0900

    :hammer: [FE] Modal 표시 창 수정

--- src/components/modal/Modal.js
+++ src/components/modal/Modal.js
@@ -1,17 +1,28 @@
 import React from 'react';
 import './Modal.css';
 
-function Modal({ message, onClose }) {
+function ResponseModal({ show, onHide, responseData }) {
+  if (!show) return null;
+
   return (
-    <div className="modal-container">
-      <div className="modal-box">
-        <div className="modal-content">
-          <p>{message}</p>
-          <button onClick={onClose}>Close</button>
+    <div className="modal-overlay">
+      <div className="modal-content">
+        <div className="modal-header">
+          <h2 className="modal-title">채점 결과</h2>
+        </div>
+        <div className="modal-body">
+          {responseData.correct ? (
+            <p className="correct-text">정답</p>
+          ) : (
+            <p className="incorrect-text">오답</p>
+          )}
+        </div>
+        <div className="modal-footer">
+          <button className="close-button" onClick={onHide}>닫기</button>
         </div>
       </div>
     </div>
   );
 }
 
-export default Modal;
+export default ResponseModal;
