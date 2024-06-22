2582a87a24b6dddcb4fd40f33f1b00ae9f407b62
mingi choo <cn78943@gmail.com>
2024-06-22 23:28:34 +0900

    :hammer: [FE] 배포된 백엔드측으로 코드 변경

--- src/chat/hooks/UseWebSocket.js
+++ src/chat/hooks/UseWebSocket.js
@@ -11,8 +11,9 @@ const UseWebSocket = () => {
   const stompClient = useRef(null);
 
   const connect = useCallback(() => {
-    const socket = new SockJS("http://localhost:8080/chat");
+    const socket = new SockJS("https://k618de24a93cca.user-app.krampoline.com/chat");
     stompClient.current = Stomp.over(socket);
+    console.log("socket :", socket);
 
     stompClient.current.connect({}, (frame) => {
       console.log("connected: ", frame);
