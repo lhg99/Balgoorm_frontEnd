0836172a72299cb622d2a4e7addb86800913598c
mingi choo <cn78943@gmail.com>
2024-06-22 19:37:44 +0900

    :lipstick: [FE] CSS 간단 수정

--- src/components/icons/ArrowLeftIcon.js
+++ src/components/icons/ArrowLeftIcon.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const ArrowLeftIcon = ({ onClick }) => (
-  <svg onClick={onClick} xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="blue" className="bi bi-arrow-left" viewBox="0 0 16 16">
+  <svg onClick={onClick} xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#7bbdff" className="bi bi-arrow-left" viewBox="0 0 16 16">
     <path fillRule="evenodd" d="M15 8a.5.5 0 0 1-.5.5H2.707l3.147 3.146a.5.5 0 0 1-.708.708l-4-4a.5.5 0 0 1 0-.708l4-4a.5.5 0 0 1 .708.708L2.707 7.5H14.5a.5.5 0 0 1 .5.5z" />
   </svg>
 );
