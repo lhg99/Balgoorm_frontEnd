71c780b087c8de2c97b8e70ccc0062b996915f3e
lhg99 <lee991229@naver.com>
2024-06-23 17:13:39 +0900

    :bug: [FE] 회원탈퇴 기능 안됨

--- src/user/DeleteAccount.js
+++ src/user/DeleteAccount.js
@@ -3,28 +3,47 @@
  * 제대로 동작하는지 확인해야됨
  */
 
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Col, Container, Form, Row } from 'react-bootstrap';
 import logo1 from '../img/Logo1.png';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 import { useAuth } from './auth/AuthContext.js';
+import Cookies from 'js-cookie';
 
 function DeleteAccount() {
   const [password, setPassword] = useState('');
-  const { user} = useAuth();
+  const { user } = useAuth();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!user) {
+      console.log('사용자 정보를 불러오는 중...');
+    } else {
+      console.log('사용자 정보:', user);
+    }
+  }, [user]);
 
   const handleDelete = async () => {
     if(!password) {
+      alert('비밀번호를 입력하세요');
+      return;
+    }
+
+    if(!user || user.userId) {
+      console.log("유저 정보 불러오기 실패");
+    }
+    
+    if(password) {
       try {
-        await axios.delete(`http://localhost:8080/deleteUser/${user.id}`, {data: {password}});
-        localStorage.removeItem('token');
-        localStorage.removeItem('role');
+        await axios.delete(`http://localhost:8080/api/deleteUser/${user.userId}`, {data: user.userId});
+        Cookies.remove('token');
+        Cookies.remove('role');
         alert("계정이 삭제되었습니다. 그동안 저희 서비스를 이용해주셔서 감사합니다.");
-        navigate('/login')
+        navigate('/login');
       } catch (error) {
-        alert('계정 삭제 실패', error);
+        // alert('계정 삭제 실패', error);
+        console.log("계정 삭제 실패", error);
       }
     }
   }
